b10a07939360b20073e5d0725bb1e634909f77da
Mike McGowan <mike.mcgowan.75@gmail.com>
2024-12-01 07:13:59 +0000

    Tidy

--- spec/day01_spec.lua
+++ spec/day01_spec.lua
@@ -2,10 +2,10 @@ local day = require("day01")
 
 describe("day 1", function()
   it("works for part 1", function()
-    assert.are.same(11, day.part1("./inputs/day01-test.txt"))
+    assert.are.same(11, day.part1("./inputs/day01-spec.txt"))
   end)
 
   it("works for part 2", function()
-    assert.are.same(31, day.part2("./inputs/day01-test.txt"))
+    assert.are.same(31, day.part2("./inputs/day01-spec.txt"))
   end)
 end)
